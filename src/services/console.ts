60d42576e4ed1d09364a777cc5bdaf5687a3ffdc
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-08-09 14:44:57 +0000

    update template console

--- src/services/console.ts
+++ src/services/console.ts
@@ -1,9 +1,23 @@
 import { Injectable, LoggerService } from '@nestjs/common'
 import { Console } from '@esliph/util-node'
 
-export class ConsoleLogger extends Console {
+const TEMPLATE_LOG = '<prefix?value="#"&styles=italic> <met?value="  LOG  "&background=blue> <dateTime> <context?color=green&styles=bold>: <message>'
+const TEMPLATE_ERROR = '<prefix?value="#"&styles=italic> <met?value=" ERROR "&background=red> <dateTime> <context?color=green&styles=bold>: <message>'
+const TEMPLATE_WARN =
+    '<prefix?value="#"&styles=italic> <met?value=" WARN  "&background=yellow&color=black> <dateTime> <context?color=green&styles=bold>: <message>'
+const TEMPLATE_INFO = '<prefix?value="#"&styles=italic> <met?value=" INFO  "&background=white> <dateTime> <context?color=green&styles=bold>: <message>'
+
+export class ConsoleLogger extends Console<typeof TEMPLATE_LOG, typeof TEMPLATE_ERROR, typeof TEMPLATE_WARN, typeof TEMPLATE_INFO> {
     constructor(args?: { context?: string }) {
-        super({ methodsValue: { ...args, pidName: CONSOLE_PID_NAME } })
+        super({
+            methodsValue: { ...args },
+            templates: {
+                error: TEMPLATE_ERROR,
+                log: TEMPLATE_LOG,
+                info: TEMPLATE_INFO,
+                warn: TEMPLATE_WARN
+            }
+        })
     }
 }
 
